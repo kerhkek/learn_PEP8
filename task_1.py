b547e2bcaacbc59c661cd0992f92693c9bd7599a
kerhkek <arina.belik.29@mail.ru>
2026-03-19 16:53:43 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -3,5 +3,5 @@ def calculate(x, y):
     result = 0
     for i in range(0, 10):
         if i % 2 == 0:
-            result += math.pow(x,i) / math.factorial(i)
+            result += math.pow(x, i) / math.factorial(i)
     return result + y
