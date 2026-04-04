7804f194af09173d48c9fcd1693526b38c956b50
kerhkek <arina.belik.29@mail.ru>
2026-04-04 09:50:18 +0700

    Update task_9.py

--- task_9.py
+++ task_9.py
@@ -1,7 +1,9 @@
 numbers = list(range(1, 11))
 squares = [n * n for n in numbers if n % 2 == 0]
 odds = [ n for n in numbers if n % 2 != 0]
+
 def generate_data(n):
     return [i * 2 for i in range(1, n)]
+    
 data = generate_data(100)
 filtered = [x for x in data if x % 10 == 0]
