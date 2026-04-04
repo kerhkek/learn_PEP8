260519718c0a6ef9f6c51f37a7f1aa3f21b49ba9
kerhkek <arina.belik.29@mail.ru>
2026-04-04 09:49:47 +0700

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,16 +1,22 @@
 class Animal:
+    
     def __init__(self, name, age, weight):
         self.name = name
         self.age = age
         self.weight = weight
+        
     def eat(self, food):
         self.weight += food
         return self.weight
+        
     def info(self):
         print(f' {self.name}, {self.age}, {self.weight} kg')
+        
 class Cat(Animal):
+    
     def __init__(self, name, age, weight, breed):
         super().__init__(name, age, weight)
         self.breed = breed
+        
     def bark(self):
         print('MEOW!')
